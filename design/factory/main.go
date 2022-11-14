e7e1738ae7bd3ec5580b5cb234074292ab11bf9c
lei <imleilei123@gmail.com>
2022-11-14 10:29:25 +0800

    feat: design

--- design/factory/main.go
+++ design/factory/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 /*一.简单工厂:缺点就是生产的产品如果要扩充,则必须修改工厂函数,增加case
-产品过多就会臃肿.*/
+产品过多就会臃肿*/
 
-// Printer 代表产品接口.
+// Printer 代表产品接口
 type Printer interface {
 	Print() string
 }
@@ -21,14 +21,16 @@ func NewPrinter(lan string) Printer {
 	}
 }
 
-// 具体的产品实现.
-type chPrinter struct{}
+// 具体的产品实现
+type chPrinter struct {
+}
 
 func (c chPrinter) Print() string {
 	return "你好"
 }
 
-type enPrinter struct{}
+type enPrinter struct {
+}
 
 func (c enPrinter) Print() string {
 	return "hello"
@@ -39,39 +41,40 @@ func (c enPrinter) Print() string {
 以解决简单工厂生产太多产品时导致其内部代码臃肿（switch … case分支过多）的问题
 每个工厂的实现,只能创建一个具体的产品,实现多种的产品,需要拓展对应的工厂实现
 */
+//Go中没有继承，所以这里说的工厂子类，其实是直接实现工厂接口的具体工厂类
 
-
-// OperatorFactory 工厂接口,即能够生产指定类别的产品,才能实现此接口.
+// OperatorFactory 工厂接口,即能够生产指定类别的产品,才能实现此接口
 type OperatorFactory interface {
 	Create() MathOperator
 }
 
-// MathOperator 代表着产品接口,产品即是具有同类行为的.
+// MathOperator 代表着产品接口,产品即是具有同类行为的
 type MathOperator interface {
 	SetOperandA(int)
 	SetOperandB(int)
 	ComputeResult() int
 }
 
+//假设要生产2类计算器,加法和乘法计算器,实现两个工厂类
 
-
-// PlusOperatorFactory 加法计算器的生产工厂.
-type PlusOperatorFactory struct{}
-
-func (p PlusOperatorFactory) Create() MathOperator {
-
+// PlusOperatorFactory 加法计算器的生产工厂
+type PlusOperatorFactory struct {
 }
 
-// PlusOperator 加法计算器具体的产品.
-type PlusOperator struct {
+func (p PlusOperatorFactory) Create() MathOperator {
+	return &PlusOperator{BaseOperator: &BaseOperator{}} //此处实际开发中会填充复杂的初始化逻辑
+}
 
+// PlusOperator 加法计算器具体的产品
+type PlusOperator struct {
+	*BaseOperator //匿名嵌套基类,公用公共方法
 }
 
 func (p PlusOperator) ComputeResult() int {
 	return p.operandA + p.operandB
 }
 
-// BaseOperator 因为有些公共的方法在不同的产品中很可能是重复的,因此将公共方法抽象为基类,多种实现直接继承即可.
+// BaseOperator 因为有些公共的方法在不同的产品中很可能是重复的,因此将公共方法抽象为基类,多种实现直接继承即可
 type BaseOperator struct {
 	operandA, operandB int
 }
@@ -84,16 +87,16 @@ func (o *BaseOperator) SetOperandB(operand int) {
 	o.operandB = operand
 }
 
-// MultiOperatorFactory 乘法计算器工厂.
+// MultiOperatorFactory 乘法计算器工厂
 type MultiOperatorFactory struct{}
 
 func (mf *MultiOperatorFactory) Create() MathOperator {
 	return &MultiOperator{
-
+		BaseOperator: &BaseOperator{}, //注意 实际是复杂初始化逻辑
 	}
 }
 
-// MultiOperator 实际的产品类--乘法运算器.
+// MultiOperator 实际的产品类--乘法运算器
 type MultiOperator struct {
 	*BaseOperator
 }
@@ -106,9 +109,9 @@ func (m *MultiOperator) ComputeResult() int {
 // 灵活性增强，对于新产品的创建，只需多写一个相应的工厂类
 // 典型的解耦框架。高层模块只需要知道产品的抽象类，无须关心其他实现类，满足迪米特法则、依赖倒置原则和里氏替换原则。
 // 缺点: 只能生产一种具有相同行为的产品，此弊端可使用抽象工厂模式解决
-// 工厂方法可用于分层架构,如控制层依赖服务层的工厂方法,而服务层又依赖仓库层的工厂方法,每一层都依赖于下一层的接口.
+// 工厂方法可用于分层架构,如控制层依赖服务层的工厂方法,而服务层又依赖仓库层的工厂方法,每一层都依赖于下一层的接口
 func main() {
-
+	//一.工厂方法使用
 	//1.初始化工厂的实现(依赖注入)
 	var factory OperatorFactory
 	factory = &PlusOperatorFactory{}
@@ -117,7 +120,7 @@ func main() {
 	plus.SetOperandB(1000)
 	fmt.Println("plus:", plus.ComputeResult())
 
-
+	//乘法计算器生产
 	factory = &MultiOperatorFactory{}
 	m := factory.Create()
 	m.SetOperandB(100)
@@ -125,7 +128,7 @@ func main() {
 
 	fmt.Println("mu:", m.ComputeResult())
 
-
+	//二.抽象工厂使用
 
 }
 
@@ -135,5 +138,5 @@ func main() {
 而抽象工厂模式则需要面对多个产品等级结构，一个工厂等级结构可以负责多个不同产品等级结构中的产品对象的创建 。
 工厂方法模式只有一个抽象产品类，而抽象工厂模式有多个。工厂方法模式的具体工厂类只能创建一个具体产品类的实例，而抽象工厂模式可以创建多个
 
-缺点是规定了所有可能被创建的产品集合，产品族中如果扩展新的产品(增加工厂接口的方法列表)，则需要修改抽象工厂的接口.
+缺点是规定了所有可能被创建的产品集合，产品族中如果扩展新的产品(增加工厂接口的方法列表)，则需要修改抽象工厂的接口
 */
--- design/strategy/main.go
+++ design/strategy/main.go
@@ -1,20 +1,69 @@
 package main
 
+import "fmt"
+
+/*
+策略模式经常会和模板模式配合使用,注意区分他们的区别,模板模式是针对一个流程共性梳理出的固定的执行步骤,具体的
+执行方式由子类来实现
+而策略模式是侧重于让完成某个任务的具体方式可以相互切换
+两个模式的解耦维度不同,策略模式在抽象方法的实现里,经常会用到模板模式
+如,多种支付策略实际他们的流程都是类似的,都是查询余额 确认金额,扣款等,不同的策略实现中,就可以将这个支付过程
+抽象为一个模板,即使是不同的策略,也会出现流程一致的情况
+
+
+*/
+
 func main() {
+	wxPay := &WxPay{}
+	px := NewPayCtx(wxPay) //设置微信支付策略,此处设置哪一种策略可以增加逻辑判断从而实现动态的策略选择
+	px.Pay()
+
 }
 
-
-	Start() string
+// PayBehavior 策略接口
+type PayBehavior interface {
+	OrderPay(ps *PayCtx)
 }
 
-type operator struct {
-	s scanStrategy
+type WxPay struct {
 }
 
-func (o operator) Start() string {
-	return o.s.Start()
+func (w WxPay) OrderPay(px *PayCtx) {
+	fmt.Printf("Wx支付加工支付请求 %v\n", px.payParams)
+	fmt.Println("正在使用Wx支付进行支付")
 }
 
-func (o operator) setStrategy(s scanStrategy) {
-	o.s = s
+// 三方支付
+type ThirdPay struct{}
+
+func (*ThirdPay) OrderPay(px *PayCtx) {
+	fmt.Printf("三方支付加工支付请求 %v\n", px.payParams)
+	fmt.Println("正在使用三方支付进行支付")
+}
+
+// PayCtx 代表策略执行所需的上下文,由其维护策略及其参数
+type PayCtx struct {
+	// 提供支付能力的接口实现
+	payBehavior PayBehavior
+	// 支付参数
+	payParams map[string]interface{}
+}
+
+func (px *PayCtx) setPayBehavior(p PayBehavior) {
+	px.payBehavior = p //设置策略
+}
+
+func (px *PayCtx) Pay() {
+	px.payBehavior.OrderPay(px)
+}
+
+func NewPayCtx(p PayBehavior) *PayCtx {
+	params := map[string]interface{}{
+		"appId": "234fdfdngj4",
+		"mchId": 123456,
+	}
+	return &PayCtx{
+		payBehavior: p,
+		payParams:   params,
+	}
 }
